a64b2cc45c47e6b421d84cf5a944068b325b18e7
Addy Ke <addy.ke@rock-chips.com>
2018-08-21 06:46:42 -0400

    compile on fedora28
    Signed-off-by: Addy Ke <addy.ke@rock-chips.com>

--- make.sh
+++ make.sh
@@ -28,7 +28,11 @@ make ARCH=arm64 rk1808-${DTB}.img -j${JOB}
 cp -f arch/arm64/boot/dts/rockchip/rk1808-${DTB}.dtb boot_linux/extlinux/rk1808.dtb
 cp -f arch/arm64/boot/Image boot_linux/extlinux/
 cp -f extlinux.conf boot_linux/extlinux/extlinux.conf
-genext2fs -b 32768 -B $((32 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
+if [ "`uname -i`" == "aarch64" ]; then
+	echo y | mke2fs -b 4096 -d boot_linux -i 8192 -t ext2 boot_linux.img $((32 * 1024 * 1024 / 4096))
+else
+	genext2fs -b 32768 -B $((32 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
+fi
 rm -rf boot_linux
 
 exit 0
--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -47,12 +47,41 @@ allowed_warnings = set([
     "memcontrol.c:5337", # mm/memcontrol.c:5337:12: warning: initialization from incompatible pointer type
     "atags_to_fdt.c:98", # arch/arm/boot/compressed/atags_to_fdt.c:98:1: warning: the frame size of 1032 bytes is larger than 1024 bytes
     "drm_edid.c:3506", # drivers/gpu/drm/drm_edid.c:3506:13: warning: 'cea_db_is_hdmi_forum_vsdb' defined but not used
-    # W=1
     "bounds.c:15", # kernel/bounds.c:15:6: warning: no previous prototype for ‘foo’
     "cpufeature.h:157", # arch/arm64/include/asm/cpufeature.h:157:68: warning: signed and unsigned type in conditional expression
     "sched.h:1211", # include/linux/sched.h:1211:1: warning: type qualifiers ignored on function return type
     "halphyrf_8188e_ce.c:2208", # drivers/net/wireless/rockchip_wlan/rtl8189es/hal/phydm/rtl8188e/halphyrf_8188e_ce.c:2208:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
     "halphyrf_8723b_ce.c:2879", # drivers/net/wireless/rockchip_wlan/rtl8723bu/hal/phydm/rtl8723b/halphyrf_8723b_ce.c:2879:1: warning: the frame size of 1056 bytes is larger than 1024 bytes
+    "vdso.c:119", # arch/arm64/kernel/vdso.c:119:6: warning: ‘memcmp’ reading 4 bytes from a region of size 1 [-Wstringop-overflow=]
+    "syscalls.h:195", # include/linux/syscalls.h:195:18: warning: ‘sys_set_tid_address’ alias between functions of incompatible types ‘long int(int *)’ and ‘long int(long int)’ [-Wattribute-alias]
+    "compat.h:48", # include/linux/compat.h:48:18: warning: ‘compat_sys_sysctl’ alias between functions of incompatible types ‘long int(struct compat_sysctl_args *)’ and ‘long int(long int)’ [-Wattribute-alias]
+    "exec.c:1084", # fs/exec.c:1084:32: warning: argument to ‘sizeof’ in ‘strncpy’ call is the same expression as the source; did you mean to use the size of the destination? [-Wsizeof-pointer-memaccess]
+    "regcache-rbtree.c:36", # drivers/base/regmap/regcache-rbtree.c:36:1: warning: alignment 1 of ‘struct regcache_rbtree_node’ is less than 8 [-Wpacked-not-aligned]
+    "dhd_linux.c:6913", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c:6913:39: warning: argument to ‘sizeof’ in ‘strncpy’ call is the same expression as the source; did you mean to use the size of the destination? [-Wsizeof-pointer-memaccess]
+    "dhd_linux.c:9284", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c:9284:4: warning: ‘strncat’ specified bound 2 equals source length [-Wstringop-overflow=]
+    "dhd_linux.c:15081", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c:15081:2: warning: ‘strncpy’ output truncated before terminating nul copying 11 bytes from a string of the same length [-Wstringop-truncation]
+    "bcmutils.h:39", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/include/bcmutils.h:39:57: warning: ‘strncpy’ output truncated before terminating nul copying 45 bytes from a string of the same length [-Wstringop-truncation]
+    "wl_android.c:2977", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_android.c:2977:2: warning: ‘strncpy’ output truncated before terminating nul copying 3 bytes from a string of the same length [-Wstringop-truncation]
+    "wl_cfg80211.c:3522", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_cfg80211.c:3522:3: warning: ‘strncpy’ output truncated before terminating nul copying 3 bytes from a string of the same length [-Wstringop-truncation]
+    "scsi.c:555", # drivers/nvme/host/scsi.c:555:2: warning: ‘strncpy’ output truncated before terminating nul copying 8 bytes from a string of the same length [-Wstringop-truncation]
+    "sctp.h:306", # include/uapi/linux/sctp.h:306:1: warning: alignment 4 of ‘struct sctp_paddr_change’ is less than 8 [-Wpacked-not-aligned]
+    "sctp.h:580", # include/uapi/linux/sctp.h:580:1: warning: alignment 4 of ‘struct sctp_setpeerprim’ is less than 8 [-Wpacked-not-aligned]
+    "sctp.h:579", # include/uapi/linux/sctp.h:579:26: warning: ‘sspp_addr’ offset 4 in ‘struct sctp_setpeerprim’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "sctp.h:593", # include/uapi/linux/sctp.h:593:1: warning: alignment 4 of ‘struct sctp_prim’ is less than 8 [-Wpacked-not-aligned]
+    "sctp.h:592", # include/uapi/linux/sctp.h:592:26: warning: ‘ssp_addr’ offset 4 in ‘struct sctp_prim’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "sctp.h:640", # include/uapi/linux/sctp.h:640:1: warning: alignment 4 of ‘struct sctp_paddrparams’ is less than 8 [-Wpacked-not-aligned]
+    "sctp.h:634", # include/uapi/linux/sctp.h:634:26: warning: ‘spp_address’ offset 4 in ‘struct sctp_paddrparams’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "sctp.h:747", # include/uapi/linux/sctp.h:747:1: warning: alignment 4 of ‘struct sctp_paddrinfo’ is less than 8 [-Wpacked-not-aligned]
+    "sctp.h:741", # include/uapi/linux/sctp.h:741:26: warning: ‘spinfo_address’ offset 4 in ‘struct sctp_paddrinfo’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "compat.c:518", # net/compat.c:518:1: warning: alignment 4 of ‘struct compat_group_req’ is less than 8 [-Wpacked-not-aligned]
+    "compat.c:516", # net/compat.c:516:35: warning: ‘gr_group’ offset 4 in ‘struct compat_group_req’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "compat.c:526", # net/compat.c:526:1: warning: alignment 4 of ‘struct compat_group_source_req’ is less than 8 [-Wpacked-not-aligned]
+    "compat.c:522", # net/compat.c:522:35: warning: ‘gsr_group’ offset 4 in ‘struct compat_group_source_req’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "compat.c:524", # net/compat.c:524:35: warning: ‘gsr_source’ offset 132 in ‘struct compat_group_source_req’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "compat.c:536", # net/compat.c:536:1: warning: alignment 4 of ‘struct compat_group_filter’ is less than 8 [-Wpacked-not-aligned]
+    "compat.c:530", # net/compat.c:530:35: warning: ‘gf_group’ offset 4 in ‘struct compat_group_filter’ isn’t aligned to 8 [-Wpacked-not-aligned]
+    "dhd_linux.c:6965", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c:6965:39: warning: argument to 'sizeof' in 'strncpy' call is the same expression as the source; did you mean to use the size of the destination? [-Wsizeof-pointer-memaccess]
+    "dhd_linux.c:9447", # drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c:9447:4: warning: 'strncat' specified bound 2 equals source length [-Wstringop-overflow=]
  ])
 
 # Capture the name of the object file, can find it.
