05fb82836059ff228de662cde461e14fb8ec9f3e
chensq <chensq@rock-chips.com>
2018-08-30 09:51:06 +0800

    arm64: driver:modify default hdmi out display(1080p)

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -1433,9 +1433,9 @@ static int drm_fb_helper_single_fb_probe(struct drm_fb_helper *fb_helper,
 	if (crtc_count == 0 || sizes.fb_width == -1 || sizes.fb_height == -1) {
 		/* hmm everyone went away - assume VGA cable just fell out
 		   and will come back later. */
-		DRM_INFO("Cannot find any crtc or sizes - going 1024x768\n");
-		sizes.fb_width = sizes.surface_width = 1024;
-		sizes.fb_height = sizes.surface_height = 768;
+		DRM_INFO("Cannot find any crtc or sizes - going 1920x1080\n");
+		sizes.fb_width = sizes.surface_width = 1920;
+		sizes.fb_height = sizes.surface_height = 1080;
 	}
 
 	/* push down into drivers */
