116a1a0e48df636eb425440c0d78b71641fe5e88
jax.fang <jax.fang@rock-chips.com>
2019-03-04 18:44:01 +0800

    misc: extbrd_drv: add eai610 to be compatibled

--- drivers/misc/extbrd_drv.c
+++ drivers/misc/extbrd_drv.c
@@ -47,12 +47,17 @@
 #define ARR_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
 /* Board Resource definition */
-#define TB_PROD
-//#define EXT_DIGIT_GPIOS
-//#define EXT_GPIO_KEYS
+static int board_type;
+#define BOARD_EAI610 	0x1
+#define BOARD_PROD		0x2
+#define BOARD_PROP		0x4
+
+#define EXT_DIGIT_GPIOS
+#define EXT_GPIO_KEYS
 #define EXT_ADC
 
 static const struct of_device_id of_extbrd_match[] = {
+	{ .compatible = "eai610-extboard", },
 	{ .compatible = "prod-extboard", },
 	{ .compatible = "prop-extboard", },
 	{},
@@ -74,17 +79,291 @@ struct extgpio_desc
 };
 
 static struct extgpio_desc digit_gpios[] = {
-	{RK_GPIO(1, 7), "TUBE_A"},
-	{RK_GPIO(1, 8), "TUBE_B"},
-	{RK_GPIO(1, 9), "TUBE_C"},
-	{RK_GPIO(1, 10), "TUBE_D"},
+	{RK_GPIO(2, 2), "TUBE_A"},	//GPIO2_A2
+	{RK_GPIO(2, 3), "TUBE_B"},	//GPIO2_A3
+	{RK_GPIO(2, 4), "TUBE_C"},	//GPIO2_A4
+	{RK_GPIO(2, 5), "TUBE_D"},	//GPIO2_A5
+	{RK_GPIO(2, 6), "TUBE_E"},	//GPIO2_A6
+	{RK_GPIO(1, 23), "TUBE_F"},	//GPIO1_C7
+	{RK_GPIO(1, 22), "TUBE_G"},	//GPIO1_C6
+	{RK_GPIO(1, 24), "TUBE_DP"},//GPIO1_D0
 };
 
 #define EXT_GPIO_NUM  	ARR_SIZE(digit_gpios)
+static int tube_set_gpio(const char *name, int value)
+{
+	int i;
+
+	for (i = 0; i < EXT_GPIO_NUM; i++) {
+		if (!strcmp(digit_gpios[i].tubename, name)) {
+			gpio_set_value(digit_gpios[i].gpio, value);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+static int display_digit_tube(const char ch)
+{
+	switch (ch) {
+		case '0':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 0\n");
+			break;
+		case '1':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 1\n");
+			break;
+		case '2':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 2\n");
+			break;
+		case '3':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 3\n");
+			break;
+		case '4':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 4\n");
+			break;
+		case '5':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 5\n");
+			break;
+		case '6':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 6\n");
+			break;
+		case '7':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 7\n");
+			break;
+		case '8':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 8\n");
+			break;
+		case '9':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show digit 9\n");
+			break;
+		case '.':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_LOW);
+			EXTBRD_DEBUG("show DP !\n");
+			break;
+		case 'e':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'E'!\n");
+			break;
+		case 'E':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'E'!\n");
+			break;
+		case 'f':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'F'!\n");
+			break;
+		case 'o':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'o' !\n");
+			break;
+		case 'n':
+			tube_set_gpio("TUBE_A", GPIO_LOW);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'n' !\n");
+			break;
+		case 'u':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'u' !\n");
+			break;
+		case 'U':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_LOW);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show 'U' !\n");
+			break;
+		case '-':
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_HIGH);
+			tube_set_gpio("TUBE_C", GPIO_HIGH);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_HIGH);
+			tube_set_gpio("TUBE_F", GPIO_HIGH);
+			tube_set_gpio("TUBE_G", GPIO_HIGH);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("show no digit !\n");
+			break;
+		case 'h':
+		default:
+			tube_set_gpio("TUBE_A", GPIO_HIGH);
+			tube_set_gpio("TUBE_B", GPIO_LOW);
+			tube_set_gpio("TUBE_C", GPIO_LOW);
+			tube_set_gpio("TUBE_D", GPIO_HIGH);
+			tube_set_gpio("TUBE_E", GPIO_LOW);
+			tube_set_gpio("TUBE_F", GPIO_LOW);
+			tube_set_gpio("TUBE_G", GPIO_LOW);
+			tube_set_gpio("TUBE_DP", GPIO_HIGH);
+			EXTBRD_DEBUG("Not suppored Character !\n");
+			break;
+	}
+
+	return 0;
+}
+
+static ssize_t show_digit_tube(struct device *dev,
+    struct device_attribute *attr, char *buf)
+{
+    return snprintf(buf, 32, "echo [0-9]/-/. > digit_tube\n");
+}
+
+static ssize_t store_digit_tube(struct device *dev,
+ 	struct device_attribute *attr, const char *buf, size_t count)
+{
+	display_digit_tube(buf[0]);
+
+	return count;
+}
+
+const struct device_attribute digit_tube_attrs = {
+	.attr = {
+		.name = "digit_tube",
+		.mode = 0644,
+	},
+	.show = show_digit_tube,
+	.store = store_digit_tube,
+};
 #endif /* End of EXT_DIGIT_GPIOS */
 
 #ifdef EXT_GPIO_KEYS
 static int *gpio_keys;
+
+static int eai610_gpio_keys[] = {
+	RK_GPIO(2, 12),
+	RK_GPIO(4, 24),
+};
 
 static int prod_gpio_keys[] = {
 	RK_GPIO(1, 10), //GPIO1_B2
@@ -102,6 +381,13 @@ static int prop_gpio_keys[] = {
 static struct iio_channel *adc_chan_map;
 
 static int *ext_gpio_leds;
+static int ext_gpio_leds_num;
+
+static int eai610_ext_gpio_leds[] = {
+	RK_GPIO(4, 29), //GPIO4_D5
+	RK_GPIO(4, 26), //GPIO4_D2
+};
+
 static int prod_ext_gpio_leds[] = {
 	RK_GPIO(0, 5), //GPIO0_A5
 	RK_GPIO(1, 9), //GPIO1_B1
@@ -144,10 +430,14 @@ static void adc_key_poll(struct work_struct *work)
 				//EXTBRD_DEBUG("Chan[%d] : %d\n", i, result);
 				if (result >= ADC_VALUE_LOW && result < DRIFT_DEFAULT_ADVALUE) {
 					gpio_set_value(ext_gpio_leds[i], GPIO_LOW);
-					gpio_set_value(ext_gpio_leds[2 + i], GPIO_LOW);
+					if (board_type == BOARD_PROD || board_type == BOARD_PROP) {
+						gpio_set_value(ext_gpio_leds[2 + i], GPIO_LOW);
+					}
 				} else {
 					gpio_set_value(ext_gpio_leds[i], GPIO_HIGH);
-					gpio_set_value(ext_gpio_leds[2 + i], GPIO_HIGH);
+					if (board_type == BOARD_PROD || board_type == BOARD_PROP) {
+						gpio_set_value(ext_gpio_leds[2 + i], GPIO_HIGH);
+					}
 				}
 			}
 			ddata->result[i] = result;  //adc key button pressed, ground connect.
@@ -231,19 +521,33 @@ static int extbrd_probe(struct platform_device *pdev)
 		np = pdev->dev.of_node;
 		if (of_device_is_compatible(np, "prod-extboard")) {
 			EXTBRD_DEBUG("This is Prod extboard.\n");
+			board_type = BOARD_PROD;
 #ifdef EXT_GPIO_KEYS
 			gpio_keys = prod_gpio_keys;
 #endif
 #ifdef EXT_ADC
 			ext_gpio_leds = prod_ext_gpio_leds;
+			ext_gpio_leds_num = ARR_SIZE(prod_ext_gpio_leds);
 #endif
 		} else if (of_device_is_compatible(np, "prop-extboard")) {
 			EXTBRD_DEBUG("This is Prop extboard.\n");
+			board_type = BOARD_PROP;
 #ifdef EXT_GPIO_KEYS
 			gpio_keys = prop_gpio_keys;
 #endif
 #ifdef EXT_ADC
 			ext_gpio_leds = prop_ext_gpio_leds;
+			ext_gpio_leds_num = ARR_SIZE(prop_ext_gpio_leds);
+#endif
+		} else if (of_device_is_compatible(np, "eai610-extboard")) {
+			EXTBRD_DEBUG("This is EAI610 extboard.\n");
+			board_type = BOARD_EAI610;
+#ifdef EXT_GPIO_KEYS
+			gpio_keys = eai610_gpio_keys;
+#endif
+#ifdef EXT_ADC
+			ext_gpio_leds = eai610_ext_gpio_leds;
+			ext_gpio_leds_num = ARR_SIZE(eai610_ext_gpio_leds);
 #endif
 		} else {
 			EXTBRD_ERROR("Unsupport extbord!\n");
@@ -262,23 +566,29 @@ static int extbrd_probe(struct platform_device *pdev)
 
 	/**************** Parst ExtGPIO Begin ****************/
 #ifdef EXT_DIGIT_GPIOS
-	for (i = 0; i < EXT_GPIO_NUM; i++) {
-		if (!gpio_is_valid(digit_gpios[i].gpio)) {
-			EXTBRD_ERROR("Invalid gpio : %d\n", digit_gpios[i].gpio);
-			ret = -EINVAL;
+	if (board_type == BOARD_EAI610) {
+		for (i = 0; i < EXT_GPIO_NUM; i++) {
+			if (!gpio_is_valid(digit_gpios[i].gpio)) {
+				EXTBRD_ERROR("Invalid gpio : %d\n", digit_gpios[i].gpio);
+				ret = -EINVAL;
+				return ret;
+			}
+
+			ret = devm_gpio_request(dev, digit_gpios[i].gpio, digit_gpios[i].tubename);
+			if (ret != 0) {
+				EXTBRD_ERROR("Request gpio : %d fail\n", digit_gpios[i].gpio);
+				ret = -EIO;
+				return ret;
+			}
+
+			//low : light on, high: light off
+			gpio_direction_output(digit_gpios[i].gpio, GPIO_HIGH);
+		}
+		if(device_create_file(&pdev->dev, &digit_tube_attrs)) {
+			EXTBRD_ERROR("Device Create digit_tube_attrs file fail.\n");
+			ret = -EEXIST;
 			return ret;
 		}
-
-		ret = devm_gpio_request(dev, digit_gpios[i].gpio, digit_gpios[i].tubename);
-		if (ret != 0) {
-			EXTBRD_ERROR("Request gpio : %d fail\n", digit_gpios[i].gpio);
-			ret = -EIO;
-			return ret;
-		}
-
-		//low : light on, high: light off
-		gpio_direction_output(digit_gpios[i].gpio, GPIO_LOW);
-		gpio_export(digit_gpios[i].gpio, true);
 	}
 #endif /* End of EXT_DIGIT_GPIOS */
 
@@ -287,38 +597,42 @@ static int extbrd_probe(struct platform_device *pdev)
 	/**************** Parst ExtKEY Begin ****************/
 #ifdef EXT_GPIO_KEYS
 	EXTBRD_DEBUG("%s %d\n", __func__, __LINE__);
-	for (i = 0; i < EXT_KEY_NUM; i++) {
-		if (!gpio_is_valid(gpio_keys[i])) {
-			EXTBRD_ERROR("Invalid gpio : %d\n", gpio_keys[i]);
-			ret = -EINVAL;
-			return ret;
-		}
+	if (gpio_keys != NULL) {
+		for (i = 0; i < EXT_KEY_NUM; i++) {
+			if (board_type == BOARD_PROP || board_type == BOARD_PROD)
+				break;
+			if (!gpio_is_valid(gpio_keys[i])) {
+				EXTBRD_ERROR("Invalid gpio : %d\n", gpio_keys[i]);
+				ret = -EINVAL;
+				return ret;
+			}
 
-		snprintf(gpiokey_name, 16, "extkey%d", i);
-		ret = devm_gpio_request(dev, gpio_keys[i], gpiokey_name);
-		if (ret != 0) {
-			EXTBRD_ERROR("gpio-keys: failed to request GPIO %d, error %d\n",
-				                          gpio_keys[i], ret);
-			ret = -EIO;
-			return ret;
-		}
+			snprintf(gpiokey_name, 16, "extkey%d", i);
+			ret = devm_gpio_request(dev, gpio_keys[i], gpiokey_name);
+			if (ret != 0) {
+				EXTBRD_ERROR("gpio-keys: failed to request GPIO %d, error %d\n",
+											  gpio_keys[i], ret);
+				ret = -EIO;
+				return ret;
+			}
 
-		ret = gpio_direction_input(gpio_keys[i]);
-		if (ret < 0) {
-			EXTBRD_ERROR("gpio-keys: failed to configure input direction for GPIO %d, error %d\n",
-				gpio_keys[i], ret);
-			return ret;
-		}
-
-		irq = gpio_to_irq(gpio_keys[i]);
-		if (irq < 0) {
-			ret = irq;
-			EXTBRD_ERROR("gpio-keys: Unable to get irq number for GPIO %d, error %d\n",
+			ret = gpio_direction_input(gpio_keys[i]);
+			if (ret < 0) {
+				EXTBRD_ERROR("gpio-keys: failed to configure input direction for GPIO %d, error %d\n",
 					gpio_keys[i], ret);
-			return ret;
+				return ret;
+			}
+
+			irq = gpio_to_irq(gpio_keys[i]);
+			if (irq < 0) {
+				ret = irq;
+				EXTBRD_ERROR("gpio-keys: Unable to get irq number for GPIO %d, error %d\n",
+						gpio_keys[i], ret);
+				return ret;
+			}
+
+			gpio_export(gpio_keys[i], true);
 		}
-		EXTBRD_DEBUG("Request GPIO(%d) IRQ(%d) OK.\n", gpio_keys[i], irq);
-		gpio_export(gpio_keys[i], true);
 	}
 #endif
 
@@ -345,7 +659,7 @@ static int extbrd_probe(struct platform_device *pdev)
 		goto fail0;
 	}
 
-	for (i = 0; i < GPIO_LEDS_NUM; i++) {
+	for (i = 0; i < ext_gpio_leds_num; i++) {
 		if (!gpio_is_valid(ext_gpio_leds[i])) {
 			 EXTBRD_ERROR("Invalid Ext gpio leds Gpio : %d\n", ext_gpio_leds[i]);
 			 ret = -EINVAL;
@@ -409,7 +723,6 @@ static int extbrd_remove(struct platform_device *pdev)
 
 	iio_channel_release_all(adc_chan_map);
 #endif
-
 	return 0;
 }
 
