d96c84f0ad33e5d520154a0723209403c1b3b500
Addy Ke <addy.ke@rock-chips.com>
2019-05-09 10:57:28 +0800

    add script for compile
    Signed-off-by: Addy Ke <addy.ke@rock-chips.com>

--- make.sh
+++ make.sh
@@ -1,57 +1,35 @@
 #!/bin/bash
 
 JOB=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
-DTB=toybrick-$2
+DTB=toybrick-$1
 
 function help()
 {
-	echo "Usage: ./make.sh os board"
+	echo "Usage: ./make.sh sx"
 	echo
 	echo "Parameter:"
-	echo "1) os:    should be android or linux"
-	echo "2) board: should be prod for TB-RK3399ProD or prop for TB-RK3399ProP"
+	echo "1) sx:    should be s0, s1, s2 ..."
 	echo
-	echo "e.g. ./make.sh android prod"
-	echo "     ./make.sh android prop"
-    echo "     ./make.sh android 96ai"
-	echo "     ./make.sh linux prod"
-	echo "     ./make.sh linux prop"
-    echo "     ./make.sh linux 96ai"
+	echo "e.g. ./make.sh s0"
 }
 
-if [ $# -lt 2 ];then
+if [ $# -lt 1 ];then
 	help
 	exit 1
 fi
-if [ ! -f arch/arm64/boot/dts/rockchip/rk3399pro-${DTB}-linux.dts ]; then
+if [ ! -f arch/arm64/boot/dts/rockchip/rk1808-${DTB}.dts ]; then
 	help
 	exit 1
 fi
 
-case $1 in
-	android)
-		make rockchip_defconfig
-		make ARCH=arm64 rk3399pro-${DTB}-android.img -j${JOB}
-		;;
-	linux)
-		mkdir -p boot_linux/extlinux
-        if [ ${DTB} == "toybrick-96ai" ]; then
-            make rockchip_96ai_linux_defconfig
-        else
-            make rockchip_linux_defconfig
-        fi
-		make ARCH=arm64 rk3399pro-${DTB}-linux.img -j${JOB}
-		cp -f arch/arm64/boot/dts/rockchip/rk3399pro-${DTB}-linux.dtb boot_linux/extlinux/rk3399pro.dtb
-		cp -f arch/arm64/boot/Image boot_linux/extlinux/
-		cp -f extlinux.conf boot_linux/extlinux/
-		genext2fs -b 32768 -B $((32 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
-		rm -rf boot_linux
-		;;
-	*)
-		help
-		exit 1
-		;;
-esac
+mkdir -p boot_linux/extlinux
+make rk1808_linux_defconfig
+make ARCH=arm64 rk1808-${DTB}.img -j${JOB}
+cp -f arch/arm64/boot/dts/rockchip/rk1808-${DTB}.dtb boot_linux/extlinux/rk1808.dtb
+cp -f arch/arm64/boot/Image boot_linux/extlinux/
+cp -f extlinux.conf boot_linux/extlinux/extlinux.conf
+genext2fs -b 32768 -B $((32 * 1024 * 1024 / 32768)) -d boot_linux -i 8192 -U boot_linux.img
+rm -rf boot_linux
 
 exit 0
 
