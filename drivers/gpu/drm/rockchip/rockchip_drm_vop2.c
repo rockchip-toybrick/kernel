d96437416e24a1febc010b2967f919e767177800
Damon Ding <damon.ding@rock-chips.com>
2024-03-28 14:50:51 +0800

    drm/rockchip: vop2: add version check and comment for disabling the aclk pre auto gating
    This is a workaround for RK3528:
    
    The aclk pre auto gating function may disable the aclk
    in some unexpected cases, which detected by hardware
    automatically.
    
    For example, if the above function is enabled, the post
    scale function will be affected, resulting in abnormal
    display.
    
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    Change-Id: Ia646c77e8010d81d5da390264e61b50ae4898464

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -3193,7 +3193,20 @@ static void vop2_initial(struct drm_crtc *crtc)
 		 */
 		VOP_CTRL_SET(vop2, auto_gating_en, 0);
 
-		VOP_CTRL_SET(vop2, aclk_pre_auto_gating_en, 0);
+		/*
+		 * This is a workaround for RK3528:
+		 *
+		 * The aclk pre auto gating function may disable the aclk
+		 * in some unexpected cases, which detected by hardware
+		 * automatically.
+		 *
+		 * For example, if the above function is enabled, the post
+		 * scale function will be affected, resulting in abnormal
+		 * display.
+		 */
+		if (vop2->version == VOP_VERSION_RK3528)
+			VOP_CTRL_SET(vop2, aclk_pre_auto_gating_en, 0);
+
 		/*
 		 * Register OVERLAY_LAYER_SEL and OVERLAY_PORT_SEL should take effect immediately,
 		 * than windows configuration(CLUSTER/ESMART/SMART) can take effect according the
