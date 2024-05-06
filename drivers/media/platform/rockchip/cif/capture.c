acb14a8b5a60a03279d648781084fb92b334b001
Zefa Chen <zefa.chen@rock-chips.com>
2024-05-06 20:18:01 +0800

    media: rockchip: vicap fixes err buffer state of buffer init
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>
    Change-Id: I36207f2ba3c1c597e09cba1fe9749bfd0b0ae283

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -2704,8 +2704,6 @@ static void rkcif_assign_new_buffer_init(struct rkcif_stream *stream,
 					 __func__, __LINE__, stream->id, stream->next_buf->vb.vb2_buf.index);
 				list_del(&stream->next_buf->queue);
 				atomic_inc(&buf_stream->sub_stream_buf_cnt);
-			} else if (stream->curr_buf) {
-				stream->next_buf = stream->curr_buf;
 			}
 		}
 
