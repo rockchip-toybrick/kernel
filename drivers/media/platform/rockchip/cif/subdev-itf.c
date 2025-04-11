9ebda6ec3606091ae06ede275a7f03d6e197d403
Zefa Chen <zefa.chen@rock-chips.com>
2025-04-11 14:29:33 +0800

    media: rockchip: vicap fixes error of change to online with multi online
    Change-Id: Icb4d148def061052085fba5f6f074e5dde523414
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>

--- drivers/media/platform/rockchip/cif/subdev-itf.c
+++ drivers/media/platform/rockchip/cif/subdev-itf.c
@@ -1466,7 +1466,7 @@ static int sditf_s_rx_buffer(struct v4l2_subdev *sd,
 	}
 
 	if (dbufs->is_switch && dbufs->type == BUF_SHORT) {
-		if (stream->is_in_vblank) {
+		if (stream->is_in_vblank || !stream->dma_en) {
 			sditf_change_to_online(priv);
 			rkcif_modify_line_int(stream, false);
 			stream->is_line_inten = false;
