df8b43bb97d397191c5c108b661d0b9b23fd58b2
Zefa Chen <zefa.chen@rock-chips.com>
2025-04-27 15:33:33 +0800

    media: rockchip: vicap fixes error resume_mode state of restart from aov
    Change-Id: Ia9df46404ffaf4c4d3b54f7a1f6b177850a6700e
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -12665,6 +12665,7 @@ static void rkcif_toisp_check_stop_status(struct sditf_priv *priv,
 				} else {
 					stream->is_single_cap = false;
 					stream->is_wait_single_cap = false;
+					stream->cifdev->resume_mode = RKISP_RTT_MODE_MULTI_FRAME;
 					complete(&stream->start_complete);
 					spin_unlock_irqrestore(&stream->cifdev->stream_spinlock, flags);
 				}
@@ -14144,6 +14145,7 @@ void rkcif_irq_pingpong_v1(struct rkcif_device *cif_dev)
 					}
 				} else {
 					last_stream->is_wait_single_cap = false;
+					cif_dev->resume_mode = RKISP_RTT_MODE_MULTI_FRAME;
 					complete(&stream->start_complete);
 					spin_unlock_irqrestore(&stream->cifdev->stream_spinlock, flags);
 					if (cif_dev->switch_info.is_use_switch)
