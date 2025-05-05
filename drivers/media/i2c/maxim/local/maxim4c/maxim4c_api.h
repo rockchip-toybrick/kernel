46df6286b426446d1739a31d98e611f76a98b5d2
troy.lin <troy.lin@rock-chips.com>
2025-05-05 10:37:59 +0800

    media: i2c: maxim: enable MAXIM4C_LOCAL_DES_ON_OFF_EN by default
    Change-Id: I74ebd1710c50e52222fc0668091939fbaabd94ec
    Signed-off-by: troy.lin <troy.lin@rock-chips.com>

--- drivers/media/i2c/maxim/local/maxim4c/maxim4c_api.h
+++ drivers/media/i2c/maxim/local/maxim4c/maxim4c_api.h
@@ -25,7 +25,7 @@
 #define MAXIM4C_TEST_PATTERN		0
 
 /* Maxim Deserializer pwdn on/off enable */
-#define MAXIM4C_LOCAL_DES_ON_OFF_EN	0
+#define MAXIM4C_LOCAL_DES_ON_OFF_EN	1
 
 /* maxim4c i2c mux api */
 int maxim4c_i2c_mux_enable(maxim4c_t *maxim4c, u8 def_mask);
