92595e9b31b36e2eebf19d56ebedd10cea97e98d
troy.lin <troy.lin@rock-chips.com>
2025-06-04 09:42:10 +0800

    scripts: mkmultidtb: modify rk3588-evb1-lp4-v10 adc value to 368
    Change-Id: I8b6e86fba93047a749f9acfc893b4aa56de58eca
    Signed-off-by: troy.lin <troy.lin@rock-chips.com>

--- scripts/mkmultidtb.py
+++ scripts/mkmultidtb.py
@@ -30,7 +30,7 @@ DTBS['RK3588-YW'] = OrderedDict([
                   ('rk3588-yw-fastboard-soca-v10', '#_saradc_ch2=0#_saradc_ch3=0#_saradc_ch4=0'),
 				  ('rk3588-yw-fastboard-socb-v10', '#_saradc_ch2=4095#_saradc_ch3=0#_saradc_ch4=0'),
 				  ('rk3588-evb7-v11', '#_saradc_ch5=2047'),
-				  ('rk3588-evb1-lp4-v10', '#_saradc_ch5=0'),
+				  ('rk3588-evb1-lp4-v10', '#_saradc_ch5=368'),
 				  ('rk3588-vehicle-evb-v23', '#_saradc_ch5=2730')])
 
 def main():
