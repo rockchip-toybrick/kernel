7c9c7115dd4c10881565682f9d17a3fe4b4991fb
Zefa Chen <zefa.chen@rock-chips.com>
2025-06-19 14:19:58 +0800

    include: uapi: rk-camera-module.h add RKMODULE_SET_CMPS_MODE
    Change-Id: I73e5af5fda20a19ccb77748fb2468d73f3cc2f3b
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>

--- include/uapi/linux/rk-camera-module.h
+++ include/uapi/linux/rk-camera-module.h
@@ -226,6 +226,9 @@
 #define RKMODULE_GET_BLC_INFO  \
 	_IOR('V', BASE_VIDIOC_PRIVATE + 54, struct rkmodule_blc_info)
 
+#define RKMODULE_SET_CMPS_MODE       \
+	_IOW('V', BASE_VIDIOC_PRIVATE + 55, __u32)
+
 struct rkmodule_i2cdev_info {
 	__u8 slave_addr;
 } __attribute__ ((packed));
@@ -977,6 +980,11 @@ struct rkmodule_wb_gain_info {
 struct rkmodule_blc_info {
 	__u32 bit_width;
 	__u32 reserved[8];
+};
+
+enum rkmodule_cmps_mode {
+	CMPS_LOW_BIT_WIDTH_MODE,
+	CMPS_HIGH_BIT_WIDTH_MODE,
 };
 
 #endif /* _UAPI_RKMODULE_CAMERA_H */
