4a6a3c953f9b1ff12e836a4984a1c8c1044c850b
troy.lin <troy.lin@rock-chips.com>
2025-07-16 10:36:40 +0800

    media: rockchip: vicap: fix bugs on deserializer check rules
    [    2.801854][    T1] Unable to handle kernel NULL pointer dereference at virtual address 0000000000000000
    [    2.816668][  T210] usb 4-1: New USB device found, idVendor=093a, idProduct=2510, bcdDevice= 1.00
    [    2.831247][    T1] Mem abort info:
    [    2.831248][    T1]   ESR = 0x96000005
    [    2.831254][    T1]   EC = 0x25: DABT (current EL), IL = 32 bits
    [    2.832027][  T210] usb 4-1: New USB device strings: Mfr=1, Product=2, SerialNumber=0
    [    2.832334][    T1]   SET = 0, FnV = 0
    [    2.832661][  T210] usb 4-1: Product: USB Optical Mouse
    [    2.833187][    T1]   EA = 0, S1PTW = 0
    [    2.833189][    T1] Data abort info:
    [    2.833877][  T210] usb 4-1: Manufacturer: PixA琀
    [    2.834201][    T1]   ISV = 0, ISS = 0x00000005
    [    2.834203][    T1]   CM = 0, WnR = 0
    [    2.836457][    T1] [0000000000000000] user address but active_mm is swapper
    [    2.837077][    T1] Internal error: Oops: 96000005 [#1] PREEMPT SMP
    [    2.837630][    T1] Modules linked in:
    [    2.837960][    T1] CPU: 7 PID: 1 Comm: swapper/0 Not tainted 5.10.226 #29
    [    2.838557][    T1] Hardware name: Rockchip RK3588 TOYBRICK LP4 X10 Board (DT)
    [    2.839187][    T1] pstate: 60c00009 (nZCv daif +PAN +UAO -TCO BTYPE=--)
    [    2.839778][    T1] pc : rkcif_output_fmt_check+0x20/0x6a8
    [    2.840256][    T1] lr : rkcif_set_fmt+0x130/0x6b0
    
    Change-Id: I5eb7d87311ce295df99d6e018d3e80610d3c0af1
    Signed-off-by: troy.lin <troy.lin@rock-chips.com>

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -844,7 +844,7 @@ static void get_remote_terminal_sensor(struct rkcif_stream *stream,
 	struct media_entity *entity = &stream->vnode.vdev.entity;
 	struct media_device *mdev = entity->graph_obj.mdev;
 	struct media_entity *entity_sensor = NULL;
-	struct media_entity *entity_bridge = NULL;
+	struct media_entity *entity_des = NULL;
 	int ret;
 
 	/* Walk the graph to locate sensor nodes. */
@@ -860,15 +860,16 @@ static void get_remote_terminal_sensor(struct rkcif_stream *stream,
 	while ((entity = media_graph_walk_next(&graph))) {
 		if (entity->function == MEDIA_ENT_F_CAM_SENSOR) {
 			entity_sensor = entity;
-		} else if (entity->function == MEDIA_ENT_F_VID_IF_BRIDGE) {
-			entity_bridge = entity;
+		} else if (entity->function == MEDIA_ENT_F_VID_IF_BRIDGE &&
+			   strstr(entity->name, "-des ")) {
+			entity_des = entity;
 			break;
 		}
 	}
 	mutex_unlock(&mdev->graph_mutex);
 	media_graph_walk_cleanup(&graph);
 
-	entity = entity_bridge ? entity_bridge : entity_sensor;
+	entity = entity_des ? entity_des : entity_sensor;
 	if (entity)
 		*sensor_sd = media_entity_to_v4l2_subdev(entity);
 	else
