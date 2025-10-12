b7a9d1a7c1dc17ef714ec3a5d113cd606903fed9
Alok Tiwari <alok.a.tiwari@oracle.com>
2025-10-12 07:20:01 -0700

    UPSTREAM: drm/rockchip: vop2: use correct destination rectangle height check
    The vop2_plane_atomic_check() function incorrectly checks
    drm_rect_width(dest) twice instead of verifying both width and height.
    Fix the second condition to use drm_rect_height(dest) so that invalid
    destination rectangles with height < 4 are correctly rejected.
    
    Fixes: 604be85547ce ("drm/rockchip: Add VOP2 driver")
    Change-Id: I38bace19bf6505607b8ef78daf8fdc96245e75b5
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Reviewed-by: Andy Yan <andy.yan@rock-chips.com>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Link: https://lore.kernel.org/r/20251012142005.660727-1-alok.a.tiwari@oracle.com
    (cherry picked from commit 7f38a1487555604bc4e210fa7cc9b1bce981c40e)
    Signed-off-by: Andy Yan <andy.yan@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -5507,7 +5507,7 @@ static int vop2_plane_atomic_check(struct drm_plane *plane, struct drm_plane_sta
 		return vpstate->format;
 
 	if (drm_rect_width(src) >> 16 < 4 || drm_rect_height(src) >> 16 < 4 ||
-	    drm_rect_width(dest) < 4 || drm_rect_width(dest) < 4) {
+	    drm_rect_width(dest) < 4 || drm_rect_height(dest) < 4) {
 		DRM_ERROR("Invalid size: %dx%d->%dx%d, min size is 4x4\n",
 			  drm_rect_width(src) >> 16, drm_rect_height(src) >> 16,
 			  drm_rect_width(dest), drm_rect_height(dest));
