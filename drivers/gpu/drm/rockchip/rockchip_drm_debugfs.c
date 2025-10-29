a8ea0f0ec49e63c38795598f9c004f72e8a75873
Chaoyi Chen <chaoyi.chen@rock-chips.com>
2025-10-29 14:41:38 +0800

    Fix drivers/gpu/drm/rockchip/rockchip_drm_debugfs.c
    Signed-off-by: Chaoyi Chen <chaoyi.chen@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_debugfs.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_debugfs.c
@@ -4,6 +4,8 @@
  * Author: Sandy Huang <hjc@rock-chips.com>
  */
 
+#include "linux/types.h"
+#include "linux/vt_kern.h"
 #include <drm/drm_atomic_uapi.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
@@ -46,10 +48,12 @@ static int temp_pow(int sum, int n)
 	int i;
 	int temp = sum;
 
-	if (n < 1)
+	if (n < 1) {
 		return 1;
-	for (i = 1; i < n ; i++)
+	}
+	for (i = 1; i < n ; i++) {
 		sum *= temp;
+	}
 	return sum;
 }
 
@@ -66,30 +70,31 @@ static int rockchip_drm_dump_plane_buffer(struct vop_dump_info *dump_info, int f
 	struct drm_gem_object *obj = dump_info->fb->obj[0];
 	struct rockchip_gem_object *rk_obj = to_rockchip_obj(obj);
 
-	drm_get_format_name(dump_info->fb->format->format, &format_name);
-	strscpy(format, format_name.str, 5);
+	(void)drm_get_format_name(dump_info->fb->format->format, &format_name);
+	(void)strscpy(format, format_name.str, 5);
 
-	flags = O_RDWR | O_CREAT;
-	snprintf(file_name, 100, "%s/%s_fb-%dx%d_stride-%d_offset-%dx%d_act-%dx%d_%s%s_%d.bin",
+	flags = (int)(O_RDWR | O_CREAT);
+	(void)snprintf(file_name, 100, "%s/%s_fb-%dx%d_stride-%d_offset-%dx%d_act-%dx%d_%s%s_%d.bin",
 		 DUMP_BUF_PATH, dump_info->win_name, dump_info->fb->width, dump_info->fb->height,
-		 dump_info->fb->pitches[0], dump_info->src->x1 >> 16, dump_info->src->y1 >> 16,
-		 drm_rect_width(dump_info->src) >> 16, drm_rect_height(dump_info->src) >> 16,
+		 dump_info->fb->pitches[0], (u32)dump_info->src->x1 >> 16U, (u32)dump_info->src->y1 >> 16U,
+		 (u32)drm_rect_width(dump_info->src) >> 16U, (u32)drm_rect_height(dump_info->src) >> 16U,
 		 format, rockchip_drm_modifier_to_string(dump_info->fb->modifier), frame_count);
 
-	kvaddr = vmap(rk_obj->pages, rk_obj->num_pages, VM_MAP,
+	kvaddr = vmap(rk_obj->pages, (unsigned int)rk_obj->num_pages, VM_MAP,
 		      pgprot_writecombine(PAGE_KERNEL));
 	kvaddr_origin = kvaddr;
-	if (!kvaddr)
+	if (!kvaddr) {
 		DRM_ERROR("failed to vmap() buffer\n");
+	}
 
 	ptr = file_name;
-	file = filp_open(ptr, flags, 0644);
+	file = filp_open(ptr, flags, 420); /* 0644 */
 	if (!IS_ERR(file)) {
-		kernel_write(file, kvaddr, rk_obj->size, &pos);
-		DRM_INFO("dump file name is:%s\n", file_name);
+		(void)kernel_write(file, kvaddr, rk_obj->size, &pos);
+		(void)DRM_INFO("dump file name is:%s\n", file_name);
 		fput(file);
 	} else {
-		DRM_INFO("open %s failed\n", ptr);
+		(void)DRM_INFO("open %s failed\n", ptr);
 	}
 	vunmap(kvaddr_origin);
 
@@ -107,14 +112,15 @@ int rockchip_drm_crtc_dump_plane_buffer(struct drm_crtc *crtc)
 	drm_atomic_crtc_for_each_plane(plane, crtc) {
 		pstate = plane->state;
 		fb = pstate->fb;
-		if (!fb)
+		if (!fb) {
 			continue;
+		}
 
 		dump_info.win_name = plane->name;
 		dump_info.fb = fb;
 		dump_info.src = &pstate->src;
 
-		rockchip_drm_dump_plane_buffer(&dump_info, rockchip_crtc->vop_dump_frame_count);
+		(void)rockchip_drm_dump_plane_buffer(&dump_info, rockchip_crtc->vop_dump_frame_count);
 	}
 	rockchip_crtc->vop_dump_frame_count++;
 
@@ -148,38 +154,41 @@ rockchip_drm_dump_buffer_write(struct file *file, const char __user *ubuf,
 	struct drm_crtc *crtc = m->private;
 	char buf[14] = {};
 	int dump_times = 0;
-	int i = 0;
+	int i;
 	struct rockchip_crtc *rockchip_crtc = to_rockchip_crtc(crtc);
 
-	if (len > sizeof(buf) - 1)
+	if (len > (size_t)sizeof(buf) - (size_t)1U) {
 		return -EINVAL;
-	if (copy_from_user(buf, ubuf, len))
+	}
+	if (copy_from_user(buf, ubuf, len) != 0UL) {
 		return -EFAULT;
-	buf[len - 1] = '\0';
+	}
+	buf[len - 1UL] = '\0';
 	if (strncmp(buf, "dumpon", 6) == 0) {
 		rockchip_crtc->vop_dump_status = DUMP_KEEP;
-		DRM_INFO("keep dumping\n");
+		(void)DRM_INFO("keep dumping\n");
 	} else if (strncmp(buf, "dumpoff", 7) == 0) {
 		rockchip_crtc->vop_dump_status = DUMP_DISABLE;
-		DRM_INFO("close keep dumping\n");
+		(void)DRM_INFO("close keep dumping\n");
 	} else if (strncmp(buf, "dump", 4) == 0) {
-		if (isdigit(buf[4])) {
-			for (i = 4; i < strlen(buf); i++) {
-				dump_times += temp_pow(10, (strlen(buf)
+		if (isdigit((int)buf[4]) != 0) {
+			for (i = 4; (unsigned long)i < strlen(buf) &&
+			     (unsigned long)i < sizeof(buf); i++) {
+				dump_times += temp_pow(10, ((int)strlen(buf)
 						       - i - 1))
 						       * (buf[i] - '0');
-		}
+			}
 			rockchip_crtc->vop_dump_times = dump_times;
 		} else {
 			drm_modeset_lock_all(crtc->dev);
-			rockchip_drm_crtc_dump_plane_buffer(crtc);
+			(void)rockchip_drm_crtc_dump_plane_buffer(crtc);
 			drm_modeset_unlock_all(crtc->dev);
 		}
 	} else {
 		return -EINVAL;
 	}
 
-	return len;
+	return (ssize_t)len;
 }
 
 static const struct file_operations rockchip_drm_dump_buffer_fops = {
@@ -201,7 +210,7 @@ int rockchip_drm_add_dump_buffer(struct drm_crtc *crtc, struct dentry *root)
 	rockchip_crtc->vop_dump_status = DUMP_DISABLE;
 	rockchip_crtc->vop_dump_times = 0;
 	rockchip_crtc->vop_dump_frame_count = 0;
-	ent = debugfs_create_file("dump", 0644, vop_dump_root,
+	ent = debugfs_create_file("dump", 420, vop_dump_root, /* 0644 */
 				  crtc, &rockchip_drm_dump_buffer_fops);
 	if (!ent) {
 		DRM_ERROR("create vop_plane_dump err\n");
@@ -237,23 +246,29 @@ static ssize_t rockchip_drm_debugfs_color_bar_write(struct file *file, const cha
 	struct seq_file *s = file->private_data;
 	struct drm_crtc *crtc = s->private;
 	struct rockchip_drm_private *priv = crtc->dev->dev_private;
-	int pipe = drm_crtc_index(crtc);
-	u8 mode;
+	unsigned int pipe = drm_crtc_index(crtc);
+	enum rockchip_color_bar_mode mode;
 
-	if (len != 2) {
-		DRM_INFO("Unsupported color bar mode\n");
+	if ((int)len != 2) {
+		(void)DRM_INFO("Unsupported color bar mode\n");
 		return -EINVAL;
 	}
 
-	if (kstrtou8_from_user(ubuf, len, 0, &mode))
+	if (kstrtou8_from_user(ubuf, len, 0, (u8 *)&mode) != 0) {
 		return -EFAULT;
-
-	if (priv->crtc_funcs[pipe]->crtc_set_color_bar) {
-		if (priv->crtc_funcs[pipe]->crtc_set_color_bar(crtc, mode))
-			return -EINVAL;
 	}
 
-	return len;
+	if (pipe >= (unsigned int)ROCKCHIP_MAX_CRTC) {
+		return -EFAULT;
+	}
+
+	if (priv->crtc_funcs[pipe]->crtc_set_color_bar != NULL) {
+		if (priv->crtc_funcs[pipe]->crtc_set_color_bar(crtc, mode) != 0) {
+			return -EINVAL;
+		}
+	}
+
+	return (ssize_t)len;
 }
 
 static const struct file_operations rockchip_drm_debugfs_color_bar_fops = {
@@ -269,10 +284,11 @@ int rockchip_drm_debugfs_add_color_bar(struct drm_crtc *crtc, struct dentry *roo
 {
 	struct dentry *ent;
 
-	ent = debugfs_create_file("color_bar", 0644, root, crtc,
+	ent = debugfs_create_file("color_bar", 420, root, crtc, /* 0644 */
 				  &rockchip_drm_debugfs_color_bar_fops);
-	if (!ent)
+	if (!ent) {
 		DRM_ERROR("Failed to add color_bar for debugfs\n");
+	}
 
 	return 0;
 }
@@ -302,25 +318,40 @@ static ssize_t rockchip_drm_debugfs_regs_write(struct file *file, const char __u
 	struct seq_file *s = file->private_data;
 	struct drm_crtc *crtc = s->private;
 	struct rockchip_drm_private *priv = crtc->dev->dev_private;
-	int ret = 0, pipe = drm_crtc_index(crtc);
+	int ret;
+	unsigned int pipe = drm_crtc_index(crtc);
 	unsigned long address = 0;
 	u32 val = 0;
 	char kbuf[32];
+	int (*sscanf_p)(const char *buf, const char *fmt, ...) = sscanf;
 
-	len = min_t(size_t, len, (sizeof(kbuf) - 1));
-	if (copy_from_user(kbuf, ubuf, len))
+	len = len < (sizeof(kbuf) - 1UL) ? len : (unsigned long)(sizeof(kbuf) - 1UL);
+	if (copy_from_user(kbuf, ubuf, len) != 0UL) {
 		return -EINVAL;
+	}
 
-	kbuf[len] = 0;
-	if (sscanf(kbuf, "%lx %x", &address, &val) == -1)
+	kbuf[len] = (char)0;
+	ret = kstrtoul(kbuf, 16, &address);
+	if (ret != 0) {
 		return -EFAULT;
+	}
 
-	if (priv->crtc_funcs[pipe]->regs_write)
+	if (sscanf_p(kbuf, "%lx %x", &address, &val) == -1) {
+		return -EFAULT;
+	}
+
+	if (pipe >= (unsigned int)ROCKCHIP_MAX_CRTC) {
+		return -EFAULT;
+	}
+
+	if (priv->crtc_funcs[pipe]->regs_write != NULL) {
 		ret = priv->crtc_funcs[pipe]->regs_write(crtc, address, val);
-	if (ret)
+	}
+	if (ret != 0) {
 		return ret;
+	}
 
-	return len;
+	return (ssize_t)len;
 }
 
 static const struct file_operations rockchip_drm_debugfs_regs_write_ops = {
@@ -336,10 +367,11 @@ int rockchip_drm_debugfs_add_regs_write(struct drm_crtc *crtc, struct dentry *ro
 {
 	struct dentry *ent;
 
-	ent = debugfs_create_file("regs_write", 0644, root, crtc,
+	ent = debugfs_create_file("regs_write", 420, root, crtc, /* 0644 */
 				  &rockchip_drm_debugfs_regs_write_ops);
-	if (!ent)
+	if (!ent) {
 		DRM_ERROR("Failed to add regs_write for debugfs\n");
+	}
 
 	return 0;
 }
